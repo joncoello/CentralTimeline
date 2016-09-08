85e18344f6e94319bb7131780ac534a98e7ddf60
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-09-08 23:33:44 +0100

    add tests - can create item controller

---
+++ CentralTimelineTests/TimelineItemControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CentralTimelineTests {
+    [TestClass]
+    public class TimelineItemControllerTests {
+
+        [TestMethod]
+        public void TimelineItemController_Create() {
+
+            var item = new CentralTimeline.Models.TimelineItem();
+
+            var sut = new CentralTimeline.Controls.TimelineItemController(item);
+
+        }
+
+    }
+}
